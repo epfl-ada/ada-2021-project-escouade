f72339e114e2c991c8578a3e7f60c2c55e9c111a
Nalyem <benoit.meylan@citycable.ch>
2021-11-12 21:09:42 +0100

    Adding film release date function

--- src/ressources/WikiData_API.py
+++ src/ressources/WikiData_API.py
@@ -33,6 +33,29 @@ def get_person_gender(IMDb_ID):
     return gender
 
 
+def get_film_info(IMDb_ID):
+    query_string = """
+SELECT DISTINCT ?film ?date ?placeLabel ?filmLabel WHERE {
+  VALUES ?value {"%s"}
+  ?film wdt:P345 ?value.
+  ?film p:P577 ?release_statement.
+  ?release_statement (psv:P577/wikibase:timePrecision) 11 ;
+    ps:P577 ?date.
+    ?release_statement pq:P291 ?place.
+  ?release_statement pq:P291 wd:Q30.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+} ORDER BY ASC (?date)
+""" % IMDb_ID
+    results = return_sparql_query_results(query_string)
+    return results['results']['bindings']
+
+
+def get_film_release_date(IMDb_ID):
+    result = get_film_info(IMDb_ID)[0]
+    date = result['date']['value']
+    return date
+
+
 if __name__ == '__main__':
     get_person_info("nm0000901")
     get_person_gender("nm0000901")
