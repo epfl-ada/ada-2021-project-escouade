cd6b6a3274512539b679fdb2cbc82d224b6716e8
Nalyem <benoit.meylan@citycable.ch>
2021-12-17 15:56:04 +0100

    Commenting empath

--- src/ressources/empath_cat.py
+++ src/ressources/empath_cat.py
@@ -1,35 +1,29 @@
 import os
 from collections import defaultdict
 
-
-def default_tokenizer(doc, n = None):
-    return doc.split()
-
-
-def bigram_tokenizer(doc, n = None):
-    tokens = doc.split()
-    for t1,t2 in zip(tokens,tokens[1:]):
-        yield t1
-        yield t1+"_"+t2
-    yield tokens[-1]
-
-def n_tokenizer(doc, n):
-    tokens = doc.split()
+# Function taking a string and returning every n_gram tokens
+def n_tokenizer(string, n):
+    tokens = string.split()
     for i in range(len(tokens)):
         for j in range(i+1, min(len(tokens)+1, i+1+n)):
+            # replacing space with _ because that's how the words are stored in cat files
             yield '_'.join(tokens[i:j])
 
 class Empath:
     def __init__(self, cat_folder_path):
+        # cats loaded
         self.cats = defaultdict(list)
+        # path to the base directory (containing the categories)
         self.path = cat_folder_path
         self.staging = {}
+        # cache look up table from previous research
         self.inv_cache = {}
+        # loading category in the base directory
         for f in os.listdir(cat_folder_path):
             if len(f.split(".")) > 1 and f.split(".")[1] == "empath":
                 self.load(cat_folder_path + '/' + f)
 
-                
+    # open a tab separated file and load every word     
     def load(self,file):
         with open(file,"r") as f:
             for line in f:
@@ -40,31 +34,24 @@ class Empath:
                     self.cats[name].append(t)
                     #self.invcats[t].append(name)
 
-                    
-    def analyze(self, doc, categories = None, tokenizer = "default", normalize = False, verbose = False, debug = False):
+    # main function of class            
+    def analyze(self, doc, categories = None, tokenizer = 1, normalize = False, verbose = False):
+        # if the doc is a list, transform it to str
         if isinstance(doc,list):
             doc = "\n".join(doc)
         
-        if tokenizer == "default":
-            tokenizer = default_tokenizer
-        elif tokenizer == "bigrams":
-            tokenizer = bigram_tokenizer
-        elif isinstance(tokenizer, int):
-            n = tokenizer
-            tokenizer = n_tokenizer
-        
-        if not hasattr(tokenizer,"__call__"):
-            raise Exception("invalid tokenizer")
-        
+        # if no category given, use default categories
         if not categories:
             categories = self.cats.keys()
         
+        # if verbose = True, we save which words matche
         if verbose:
             match = []
         
         invcats = defaultdict(list)
         key = tuple(sorted(categories)) # key : catégorie(s)
         
+        # initializing the reverse dictionary that map word to category
         if key in self.inv_cache:
             invcats = self.inv_cache[key]
         else:
@@ -72,28 +59,37 @@ class Empath:
                 for t in self.cats[k]: invcats[t].append(k)
             self.inv_cache[key] = invcats
         
+        # count dict will contains category: match items
         count = {}
+        # count the total of tokens used (only usefull to normalize)
         tokens = 0.0
+        # initilaizing count to 0
         for cat in categories: count[cat] = 0.0
-        for tk in tokenizer(doc, n):
+        # iteraing through the n_gram tokens
+        for tk in n_tokenizer(doc, n):
             tokens += 1.0
+            # if the token is in the inversed dict, we add one to the count of every cat he's in
             if tk in invcats.keys():
                 for cat in invcats[tk]:
+                    # if we need to know which word matched, we add it to the match list
                     if verbose:
                         match.append(tk)
                     count[cat]+=1.0
+        # normalizing values
         if normalize:
             for cat in count.keys():
                 if tokens == 0:
                     return None
                 else:
                     count[cat] = count[cat] / tokens
+        # retunring the value, with or without match count 
         if verbose:
             return {'count': count, 'match': list(set(match))}
         else:
             return {'count': count}
 
-    
+
+    # create a category from a list of word
     def create_category(self, name, array):
         path = f"{self.path}/{name}.empath"
         array = list(set(array))
